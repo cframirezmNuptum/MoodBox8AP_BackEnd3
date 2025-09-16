452797c948bf9703080edea554b93213405b9c09
Ing-Estefania-Pachon <ing.estefaniapachon@gmail.com>
2025-09-16 12:12:20 -0500

    Ayuaaaaa

---
+++ src/test/java/com/MoodBox8ap/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MoodBox8ap.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
